0d40b85af3a1f6e7a01759bc69ec690ffdc12929
Russell Gordon <rgordon@lcs.on.ca>
2025-05-14 13:54:31 -0400

    Initial Commit

---
+++ SlideUpSheetExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideUpSheetExample
+//
+//  Created by Russell Gordon on 2025-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideUpSheetExample/SlideUpSheetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideUpSheetExampleApp.swift
+//  SlideUpSheetExample
+//
+//  Created by Russell Gordon on 2025-05-14.
+//
+
+import SwiftUI
+
+@main
+struct SlideUpSheetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
