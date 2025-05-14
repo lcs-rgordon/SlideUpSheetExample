425b63db2581dc8aa53cad56f994aaf0b9bb0c3d
Russell Gordon <rgordon@lcs.on.ca>
2025-05-14 14:36:57 -0400

    Filled out the view model to support showing of students.

--- SlideUpSheetExample/ViewModels/StudentListViewModel.swift
+++ SlideUpSheetExample/ViewModels/StudentListViewModel.swift
@@ -12,13 +12,32 @@ class StudentListViewModel: Observable {
     
     // MARK: Stored properties
     
+    // List of students, stored in the order they were added to the array
+    // NOTE: Pre-populated with example students from model
+    var students: [Student] = [example1, example2, example3, example4]
     
     // MARK: Computed properties
-    
+
+    // Return the array of students, first sorted by grade, then by last name, then by first name
+    var studentsSorted: [Student] {
+
+        // NOTE: If you are using a database (most students) the view model would
+        //       likely never need to sort an array directly. Instead, we'd
+        //       ask for the list of students from a database already sorted by
+        //       the criteria we want.
+        return students.sorted { firstStudent, secondStudent in
+            (firstStudent.currentGrade.rawValue, firstStudent.lastName, firstStudent.firstName) < (secondStudent.currentGrade.rawValue, secondStudent.lastName, secondStudent.firstName)
+        }
+    }
     
     // MARK: Initializer(s)
     
     
     // MARK: Function(s)
-    
+    func add(newStudent: Student) {
+        
+        // Append the provided student to the list of tracked students
+        self.students.append(newStudent)
+        
+    }
 }
